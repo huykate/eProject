99bcc9b187404d2b93086f396310f2eb317857f3
huykate <huyketo@gmail.com>
2024-06-25 19:59:21 +0700

    update,icon,...

---
+++ my-app/src/Header.js
@@ -0,0 +1,12 @@
+import'./App.js';
+function Header(){
+    return(
+        <div className="header">
+            <h1>jhjasdg</h1> 
+        </div>     
+    );
+       
+    
+    
+}
+export default Header;
