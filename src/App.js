757faa4f02c70f930d0888246e245448a44dbfba
huykate <huyketo@gmail.com>
2024-06-30 16:27:19 +0700

    homepage main update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import './index.css';
 import { Slide } from 'react-slideshow-image';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import 'react-slideshow-image/dist/styles.css'
 import Header from './Header.js';
 
@@ -14,35 +16,86 @@ function App() {
         <div className='Main__banner'>
           <div className='Main__banner--detail grid'>  
             <div className='Main__banner--text'>
-              <h2 className='Main__banner--text1'>Discover the beauty of 70+ years of Clarins innovation</h2>
-              <div className='Main__banner--text2'>Transform your skincare routine with Clarins' proven formulas and decades of expertise - your radiant complexion awaits!</div>
+              <h2 className='Main--text1'>Discover the beauty of 70+ years of Clarins innovation</h2>
+              <div className='Main--text2'>Transform your skincare routine with Clarins' proven formulas and decades of expertise - your radiant complexion awaits! 
+              </div>
               <button className='Main__banner--btn custom-btn '>see more</button>
             </div>
             <img  className='Main__banner--img' src={process.env.PUBLIC_URL + '../img/Banner-hero img/banner.gif'} alt=''></img>
           </div>
         </div>
-        <div className='Main__text'>
-          <h1 className='Main__text--big'>Your skin. Our expertise.</h1>
-          <h2 className='Main__text--mid'>DISCOVER OUR PLANT-POWERED FORMULAS</h2>
+        <div className='Main__title'>
+          <h1 className='Main__title--big'>Your skin. Our expertise.</h1>
+          <h2 className='Main__title--mid'>DISCOVER OUR PLANT-POWERED FORMULAS</h2>
         </div>
-        
-        <Slide cssClass='Main__category' autoplay={false} canSwipe={false} >
-            <div className='Main__category--detail'>
+        <div className='Main__slide'>
+          <Slide cssClass='Main__slide' autoplay={false} canSwipe={false} transitionDuration={500} slidesToScroll={1} slidesToShow={3} indicators={true}  >
+          <div className='Main__slide--detail'>
+            1
+          </div>
+          <div className='Main__slide--detail'>
+            2
+          </div>
+          <div className='Main__slide--detail'>
+            3
+          </div>
+          <div className='Main__slide--detail'>
+            4
+          </div>
+          <div className='Main__slide--detail'>
+            5
+          </div>
+          <div className='Main__slide--detail'>
+            6 
+          </div>
+          </Slide>
+        </div>
+        <div className='Main__title'>
+          <h1 className='Main__title--big'>Beauty must-haves</h1>
+          <h2 className='Main__title--mid'>DISCOVER OUR ICONIC PRODUCTS</h2>
+          <ButtonGroup className='Main__title--btn'  aria-label="Basic example">
+            <Button className='Main__title--btnDetail' variant="secondary">Best seller</Button>
+            <Button className='Main__title--btnDetail' variant="secondary">What's new</Button>
+            <Button className='Main__title--btnDetail' variant="secondary">Exclusive product</Button>
+          </ButtonGroup>
+          
+        <div className='Main__slide'>
+          <Slide autoplay={false} canSwipe={false} transitionDuration={500} slidesToScroll={1} slidesToShow={4}  indicators={true }>
+            <div className='Main__slide--detail'>
               <div>1</div>
-              <div>2</div>
-              <div>3</div>
             </div>
-            <div className='Main__category--detail'>
-              <div>4</div>
-              <div>5</div>
-              <div>6</div>
+            <div className='Main__slide--detail'>
+             <div>2</div>
             </div>
-        </Slide>
-        <div className='Main__text'>
-          <h1 className='Main__text--big'>Beauty must-haves</h1>
-          <h2 className='Main__text--mid'>DISCOVER OUR ICONIC PRODUCTS</h2>
+            <div className='Main__slide--detail'>
+             <div>3</div>
+            </div>
+            <div className='Main__slide--detail'>
+             <div>4</div>
+            </div>
+            <div className='Main__slide--detail'>
+             <div>5</div>
+            </div>
+            <div className='Main__slide--detail'>
+             <div>6</div>
+            </div>
+            <div className='Main__slide--detail'>
+             <div>7</div>
+            </div>
+            <div className='Main__slide--detail'>
+             <div>8</div>
+            </div>
+          </Slide>
         </div>
-        
+        </div>
+        <div className='Main__bestPro'>
+          <img className='Main__bestPro--size' src={process.env.PUBLIC_URL + '../img/Aura Perfect/Aura Perfect-1.jpeg'} alt=''></img>
+          <div className='Main__bestPro--detail' >
+            <div className='Main--text1'>Aura Perfect</div>
+            <div className='Main--text2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</div>
+            <button className='Main__bestPro--btn custom-btn'>see more</button>
+          </div>
+        </div>
       </div>
     </>
   );
