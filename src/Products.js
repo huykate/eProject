9849bce905d6bfccd6709aed81e742cca81f105b
huykate <huyketo@gmail.com>
2024-07-06 02:32:50 +0700

    product list update

--- src/Products.js
+++ src/Products.js
@@ -46,19 +46,21 @@ function ProductList() {
                 </div>
             </div>  
             <div className='grid'>
-               
-                <div className='Product'>
-                        {products.map((pro) =>(
-                        <Product 
-                            name={pro.name}
-                            img_url={pro.img_url}
-                            price={pro.price}
-                            rating={pro.rating}
-                            id={pro.id}
-                        /> ))}
-                    </div> 
+                <div className='container'>
+                    <div className='row'>
+                         {products.map((pro) =>(
+                            <div key={pro.id} className='col-12 col-sm-6 col-md-4 col-lg-3'>
+                                <Product 
+                                    name={pro.name}
+                                    img_url={pro.img_url}
+                                    price={pro.price}
+                                    rating={pro.rating}
+                                    id={pro.id}
+                                />
+                        </div>))}
+                    </div>
+                </div> 
             </div>
-            
         </>
     );    
 };
@@ -69,17 +71,20 @@ function Product({name,img_url,price,rating,id}){
     const navigate = useNavigate();
     return(
         <div onClick={() => navigate(`/Product/${id}`)}>
-            <Card className='Product__layout' >
-            <Card.Img className='Product__img'  variant="top" src={process.env.PUBLIC_URL + "../proImg/"+pic} />
-            <Card.Body >
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>
-                    {price}
-                    {rating}
-                </Card.Text>
-                <Button className='Product__btn' variant="primary">Go somewhere</Button>
-            </Card.Body>
-            </Card> 
+        <Card className='Product__layout'>
+        <Card.Img variant="top" fluid src={process.env.PUBLIC_URL + "../proImg/"+pic}  />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>
+            {price }<br />
+            <div className="product-rating">
+                {"★".repeat(Math.floor(rating)) +
+                  "☆".repeat(5 - Math.floor(rating))}
+              </div>
+          </Card.Text>
+          <Button className='Product__btn' variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
          </div>                      
 
 
